e9b251c78743ca1ab356c917fadd6c3811fd9763
zagi <biuro@zagalski.pl>
2023-12-09 13:55:24 +0100

    Missing log creation on city delete

--- server/api/city.delete.ts
+++ server/api/city.delete.ts
@@ -1,12 +1,25 @@
 import { deleteCity } from "../utils/cityService";
+import { createLog } from "../utils/logService";
 export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
   try {
-    const body = await readBody(event);
     await deleteCity(body.id);
+    createLog(
+      `/api/city`,
+      JSON.stringify(body),
+      200,
+      `Object with city.id=${body.id} and it's saved weather was deleted successfully.`
+    );
     return {
       status: "success",
     };
   } catch (error) {
+    createLog(
+      `/api/city`,
+      "",
+      500,
+      `Something went wrong with city.id=${body.id} deletion.`
+    );
     return createError({
       statusCode: 500,
     });
